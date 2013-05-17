47a5bfdfef20d85d99dbbd3fda61a0d1a6fd32b8
MengZhang <meng.zhang15@ufl.edu>
2013-05-17 01:42:09 -0400

    Add test case for meta data quick download file translator

---
+++ src/test/java/org/agmip/translators/qckdl/QckdlMetaOutputTest.java
@@ -0,0 +1,37 @@
+package org.agmip.translators.qckdl;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ *
+ * @author Meng Zhang
+ */
+public class QckdlMetaOutputTest {
+
+    QckdlMetaOutput output;
+    URL resource;
+    URL expectedRes;
+    String fileName = "metadata.json";
+
+    @Before
+    public void setUp() throws Exception {
+        output = new QckdlMetaOutput();
+        resource = this.getClass().getResource("/" + fileName);
+    }
+
+    @Test
+    public void test() throws IOException, Exception {
+
+        InputStream is = new FileInputStream(resource.getPath());
+        OutputStream os = new FileOutputStream("output/metadata.csv");
+        output.writeFile(os, is);
+    }
+}
